280f555d220acde53724df9cddb08dd23d838627
Valerii Lytvynov <siriusgti@gmail.com>
2017-08-18 23:14:48 +0300

    Update check.sh

--- bin/check.sh
+++ bin/check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 RUNNING_CONTAINERS=`docker ps | wc -l`;
-    if [ "${RUNNING_CONTAINERS//[[:space:]]/}" == "1" ]; then
+    if [ "${RUNNING_CONTAINERS//[[:space:]]/}" == "2" ]; then
       echo "[E] Container is not started\!";
       docker logs mithril_api --details --since 5h;
       exit 1;
