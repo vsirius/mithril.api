2d5889c96bf6938e89e78dafdd4cb65e1a35788d
Valerii Lytvynov <siriusgti@gmail.com>
2017-08-18 22:39:24 +0300

    Update start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -12,11 +12,11 @@ fi
 # Extract project name and version from mix.exs
 PROJECT_NAME=$(sed -n 's/.*app: :\([^, ]*\).*/\1/pg' "${PROJECT_DIR}/mix.exs")
 PROJECT_VERSION=$(sed -n 's/.*@version "\([^"]*\)".*/\1/pg' "${PROJECT_DIR}/mix.exs")
-HOST_IP=`ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' | head -n 1`
+#HOST_IP=`ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' | head -n 1`
 HOST_NAME="travis"
 
 echo "[I] Starting a Docker container '${PROJECT_NAME}' (version '${PROJECT_VERSION}') from path '${PROJECT_DIR}'.."
-echo "[I] Assigning parent host '${HOST_NAME}' with IP '${HOST_IP}'."
+#echo "[I] Assigning parent host '${HOST_NAME}' with IP '${HOST_IP}'."
 
 # Allow to pass -i option to start container in interactive mode
 OPTS="-d"
@@ -30,7 +30,7 @@ done
 docker run -p 4000:4000 \
        --env-file .env \
        ${OPTS} \
-       --add-host=$HOST_NAME:$HOST_IP \
        --name ${PROJECT_NAME} \
+       --link postgresql:travis \
        "${PROJECT_NAME}:${PROJECT_VERSION}"
 
