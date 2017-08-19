228efaaff383d5c920a6197f458728f7eba273ed
Valerii Lytvynov <siriusgti@gmail.com>
2017-08-19 17:07:29 +0300

    Create test_ci.sh

---
+++ bin/test_ci.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export DB_HOST=postgresql
+cd /mithril.api/
+mix local.rebar --force
+yes | mix deps.get 
+mix test --exclude pending --trace
+mix coveralls.travis --exclude pending
+mix credo --strict
+mix dogma
