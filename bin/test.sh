10537b86793a5be95162554fd22b04b30199fd9b
Valerii Lytvynov <siriusgti@gmail.com>
2017-08-20 14:24:25 +0300

    Update test.sh

--- bin/test.sh
+++ bin/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export DB_HOST=postgresql
-cd /test/
+cd /work/
 mix local.rebar --force
 yes | mix deps.get 
 CONTAINER_HTTP_HOST=mithril_api CONTAINER_HTTP_PORT=4000 mix test test/acceptance 
